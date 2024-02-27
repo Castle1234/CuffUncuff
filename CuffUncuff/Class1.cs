982ccdd53b881edf3438ee3dfa5228d6c6143f34
Castle1234 <149150815+Castle1234@users.noreply.github.com>
2024-02-27 22:53:22 +0000

    Add project files.

---
+++ CuffUncuff/Class1.cs
@@ -0,0 +1,68 @@
+﻿using CitizenFX.Core;
+using System;
+using CitizenFX.Core.Native;
+using System.Collections;
+
+namespace CuffUncuff
+{
+    public class Main : BaseScript
+    {
+        [Command("cuff")]
+        [Command("uncuff")]
+        
+        public void GetClosestPlayer()
+        {
+            Player closestPlayer = null;
+            float closestDistance = -1f;
+            // loop through all players
+            foreach (Player player in Players.())
+            {
+                if (player == Game.Player)
+                {
+                    continue;
+                }
+
+                var ped = player.Character;
+                var playerCoords = ped.Position;
+
+                var distance = Vector3.Distance(Game.PlayerPed.Position, playerCoords);
+
+                if (closestPlayer == null || distance < closestDistance)
+                {
+                    closestPlayer = player;
+                    closestDistance = distance;
+                }
+            }
+
+            if (closestPlayer != null)
+            {
+                CuffCommand();
+            }
+        }
+        private void CuffCommand()
+        {
+            var policePlayer = Game.PlayerPed;
+            var cuffedPlayer = Game.PlayerPed.Handle;
+            bool isCuffed = API.IsPedCuffed(cuffedPlayer);
+            //Check if the player is cuffed already
+            //Get the player closest to the police player
+            
+            switch (isCuffed)
+            {
+                case true:
+                    {
+                        //Uncuff the player
+                        API.SetEnableHandcuffs(cuffedPlayer, false);
+                        break;
+                    }
+
+                case false:
+                    {
+                        //Cuff the player
+                        API.SetEnableHandcuffs(cuffedPlayer, true);
+                        break;
+                    }
+            }
+        }
+    }
+}
